d99f08ae088065adecc6c7b656e806fff35c40c0
Andy <andy@hoard.localdomain>
2012-05-23 23:49:52 +1000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Sndstream::Application.routes.draw do
   root              to: 'static_pages#home'
+  get "users/new"
+  match '/signup',  to: 'users#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
