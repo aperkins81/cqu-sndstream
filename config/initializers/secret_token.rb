e86c624568f6e18aed3c8a04db458bb0ff54557e
Andy <andy@hoard.localdomain>
2012-05-23 17:59:19 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sndstream::Application.config.secret_token = '0bf0bb15743bea99e5ae1fcbcc79507083c7b00b6d5884ab593688de38dd51936def56faee8973fda2e482bd6028d966d37433f15725de588a3a07a25595ec37'
