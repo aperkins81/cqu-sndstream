462d9390a6aeeb6f9f953281adae64e0ebccea7e
Andy <andy@hoard.localdomain>
2012-05-24 18:50:43 +1000

    added test for mixed case email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ describe User do
     @user = User.new(name: "Example User", email: "user@example.com",
         password: "chicken", password_confirmation: "chicken")
   end
+  
   subject { @user }
   it { should respond_to :name }
   it { should respond_to :email }
@@ -24,18 +25,22 @@ describe User do
   it { should respond_to :password_confirmation }
   it { should respond_to :authenticate }
   it { should be_valid }
+  
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
   end
+  
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
+  
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+  
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[asdf@asdf,com asdf_at_asdf.com asdf@asdf. 
@@ -46,6 +51,7 @@ describe User do
       end
     end
   end
+  
   describe "when email format is valid" do
     it "should be valid" do
       addresses = %w[asdf@asdf.COM asdf_asdf-asdf@a.b.com fda.asdf@dsfds.d.foo
@@ -56,6 +62,7 @@ describe User do
       end
     end
   end
+  
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
@@ -64,14 +71,26 @@ describe User do
     end
     it { should_not be_valid }
   end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "AsDf@fSDa.cOM" }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+  
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
+  
   describe "when password does not match confirmation" do
     before { @user.password_confirmation = "not_chicken" }
     it { should_not be_valid }
   end
+  
   describe "when password confirmation is nil" do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
