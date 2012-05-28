e5f5daf0cbc658143a07f27794d3c31eec7d8123
Andy <andy@hoard.localdomain>
2012-05-28 14:03:54 +1000

    make user a partial

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user, options = { size: 50 })
+  def gravatar_for(user, options = { size: 100 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
